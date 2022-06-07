c84829754330c2ec967000341f1c2ab1f8efb8c7
Victorede <106488765+Victorede@users.noreply.github.com>
2022-06-07 05:44:00 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import *
+import pickle
+
+import pandas as pd
+
+app = Flask(__name__, static_url_path='/static')
+model = pickle.load(open('DecisionTreeClassifier_model.pkl', 'rb'))
+
+
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+
+@app.route("/", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        Age = int(request.form['Age'])
+        Gender = int(request.form['sex'])
+        Chest_Pain_type = int(request.form['Chest_Pain_type'])
+        resting_blood_pressure = int(request.form['resting_blood_pressure'])
+        cholestoral_in_mgdl = int(request.form['cholestoral_in_mg/dl'])
+        fasting_blood_sugar__120_mgdl = int(
+            request.form['fasting_blood_sugar_>_120_mg/dl'])
+        resting_electrocardiographic = int(
+            request.form['resting_electrocardiographic'])
+        maximum_heart_rate = int(request.form['maximum_heart_rate'])
+        Exercise_induced_angina = int(request.form['Exercise_induced_angina'])
+        oldpeak = float(request.form['oldpeak'])
+
+        prediction = model.predict([[Age, Gender, Chest_Pain_type, resting_blood_pressure, cholestoral_in_mgdl,
+                                   fasting_blood_sugar__120_mgdl, resting_electrocardiographic, maximum_heart_rate, Exercise_induced_angina, oldpeak]])
+
+        pred = prediction[0]
+        #out = "Error"
+        if pred == 1:
+
+            pred = "Significant risk of developing heart failure"
+        else:
+
+            pred = "less chances of developing of heart failure"
+        return render_template('index.html', results=pred)
+
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+	app.run(debug = True)
